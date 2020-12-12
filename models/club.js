a0b39d93f2b03276c0d1cf100925c9ae3031839a
Estefania <nialupo87@gmail.com>
2020-12-11 22:52:10 -0500

    added the book, club models and changed the database config

--- models/club.js
+++ models/club.js
@@ -2,6 +2,7 @@ module.exports = function (sequelize, DataTypes) {
     const CLub = sequelize.define("Club", {
         name: DataTypes.STRING,
     });
+    return CLub;
 };
 
-    return CLub;
+    
