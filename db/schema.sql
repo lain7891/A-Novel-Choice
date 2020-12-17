c7bb5325514eac2b85a4697d127553cedf7dcbaf
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-12-16 22:38:10 -0500

    placed correct password on the connectio.js and config.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,19 @@
 DROP DATABASE IF EXISTS books_db;
  
 CREATE DATABASE books_db;
+
+
+CREATE TABLE club (
+  id int AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE books (
+  id int AUTO_INCREMENT,
+  title varchar(30) NOT NULL, -- Pre-populated, and entered by user
+  autor varchar(30) NOT NULL,
+  club_id int NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(club_id) REFERENCES club(id)
+);
