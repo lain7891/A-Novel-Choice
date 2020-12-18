30c28b8d7d09c697292b7047f063133301291501
Estefania <nialupo87@gmail.com>
2020-12-17 22:19:17 -0500

    connecting to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-var mysql = require("mysql");
+var mysql2 = require("mysql2");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection = mysql2.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection = mysql.createConnection({
+	connection = mysql2.createConnection({
 	host: "localhost",
 	port: 3306,
 	user: "root",
